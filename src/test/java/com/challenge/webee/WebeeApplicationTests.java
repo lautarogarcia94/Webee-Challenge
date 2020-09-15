8e902286b86ed5809f58fa574a9cf00ec18c65d8
Lautaro Garcia <lautygcr@gmail.com>
2020-09-15 20:45:22 -0300

    Initial Commit

---
+++ src/test/java/com/challenge/webee/WebeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.webee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
