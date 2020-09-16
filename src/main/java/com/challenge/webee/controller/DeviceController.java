7d05eaf50418504d75547d77da6cae1e99de6250
Lautaro Garcia <lautygcr@gmail.com>
2020-09-16 00:41:54 -0300

    ../devices/{param} returns one Device if param is an ID or a MAC adress

--- src/main/java/com/challenge/webee/controller/DeviceController.java
+++ src/main/java/com/challenge/webee/controller/DeviceController.java
@@ -1,8 +1,11 @@
 package com.challenge.webee.controller;
 
 import com.challenge.webee.model.Device;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,8 @@ import java.util.List;
 @RestController
 @RequestMapping("devices")
 public class DeviceController {
+
+    private static final String REGEXPMAC = "^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$";
 
     /**
      * This method returns a JSON or XML formated List of saved
@@ -37,6 +42,33 @@ public class DeviceController {
         devList.add(dev);
 
         return devList;
+    }
+
+    /**
+     * @param devInfo
+     * @return
+     */
+    @GetMapping(path = "/{devInfo}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Device> getDevice(@PathVariable String devInfo) {
+        int id;
+        Device dev = new Device();
+        dev.setDate(LocalDate.now());
+        dev.setMacAdress("4F:24:AD:B1:90:21");
+        dev.setID("23");
+        if (devInfo.matches(REGEXPMAC)) {
+            dev.setMacAdress(devInfo);
+            return new ResponseEntity<Device>(dev, HttpStatus.ACCEPTED);
+        } else {
+            try {
+                id = Integer.parseInt(devInfo);
+                dev.setID(devInfo);
+                return new ResponseEntity<Device>(dev, HttpStatus.ACCEPTED);
+
+            } catch (Exception e) {
+
+            }
+        }
+        return new ResponseEntity<Device>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     
